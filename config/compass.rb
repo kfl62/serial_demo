184a87a97e15291dabf35e10e33b18d0d1c4ea0f
kfl62 <kfl62g@gmail.com>
2011-04-10 16:09:15 +0300

    configuration files

---
+++ config/compass.rb
@@ -0,0 +1,21 @@
+# encoding: utf-8
+
+if defined?(Sinatra)
+  # This is the configuration to use when running within sinatra
+  project_path = File.expand_path('..', File.dirname(__FILE__))
+  environment = :development
+else
+  # this is the configuration to use when running within the compass command line tool.
+  css_dir = File.join('public', 'stylesheets')
+  relative_assets = true
+  environment = :production
+end
+
+# This is common configuration
+sass_dir = File.join('lib', 'web_views', 'stylesheets')
+images_dir = File.join('public', 'images')
+cache_dir = File.join('tmp', 'sass-cache')
+http_path = '/'
+http_images_path = '/images'
+http_stylesheets_path = '/stylesheets'
+sass_options = {:cache_location => './tmp/sass-cache'}
